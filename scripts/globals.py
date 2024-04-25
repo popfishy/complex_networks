06e895515fd3b807981f08b712fd101c16712d98
popfishy <1301350150@qq.com>
2024-04-25 22:09:49 +0800

    修改适配参数

--- scripts/globals.py
+++ scripts/globals.py
@@ -6,12 +6,12 @@ msgGeneratorProb = 0.5
 # 毁伤——自适应恢复事件的周期
 intervalOfRemove = 200
 # 稳健性计算模型中，方窗滤波的参数
-M_WINDOW = 20
+M_WINDOW = 10
 numberOfRemovedNodesPerTime = 2
 
 
 # 考虑地理距离的无标度网络连接方式  参考p25公式：rc为通信距离
 class WireVar:
-    rc = 5.0
+    rc = 8.0
     e = 0.1
     alpha = 0.8
