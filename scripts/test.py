18b5c048d339b30b00ee997564b435cb0d0724eb
popfishy <1301350150@qq.com>
2024-04-25 22:09:58 +0800

    测试

--- scripts/test.py
+++ scripts/test.py
@@ -1,25 +1,27 @@
 # 导入库
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
+import numpy as np
 
-G = nx.Graph()
-G.add_nodes_from([0, 1, 2, 3, 4])
-G.add_edges_from([(0, 1), (0, 2), (1, 3), (1, 2), (1, 4), (3, 4)])
+matrix = np.array(
+    [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1],
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+    ]
+)
+G = nx.from_numpy_array(matrix)
 # 邻接矩阵
 As = nx.adjacency_matrix(G)
 A = As.todense()
+print(A)
 
 # 两个节点间的一条最短路径
-shortest_path = nx.shortest_path(G, source=0, target=1)
+shortest_path = nx.shortest_path(G, source=2, target=1)
 print("两个节点间的一条最短路径: ", shortest_path)
-
-# 两个节点之间所有的最短路径
-# all_shortest_path = list(nx.all_shortest_paths(G, source=0, target=3))
-# print("两个节点之间所有的最短路径: ", all_shortest_path)
-
-# # 求两个节点的最短路径长度（距离）
-# shortest_dis = nx.shortest_path_length(G, source=0, target=3)
-# print("两个节点的最短路径长度（距离）: ", shortest_dis)
-
-# nx.draw(G, node_size=1000, with_labels=True)
-# plt.show()
+print(np.sum(matrix))
+nx.draw(G, node_size=1000, with_labels=True)
+plt.show()
